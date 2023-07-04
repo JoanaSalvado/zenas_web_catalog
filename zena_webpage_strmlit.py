6583bf0326b73a17d6c6c846f55060ce03ea928f
Joana Salvado <joana.salvado@yahoo.com>
2023-07-04 16:30:03 +0100

    Update zena_webpage_strmlit.py
    test snowflake connection

--- zena_webpage_strmlit.py
+++ zena_webpage_strmlit.py
@@ -1,4 +1,15 @@
 import streamlit as st
 import snowflake.connector
+import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
+
+# Connect to Snowflake
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# Run query to test
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)

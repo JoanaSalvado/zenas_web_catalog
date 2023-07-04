c18d2b3911e337f2740e173e4a0d0b24fadd4a5f
Joana Salvado <joana.salvado@yahoo.com>
2023-07-04 16:47:58 +0100

    Update zena_webpage_strmlit.py

--- zena_webpage_strmlit.py
+++ zena_webpage_strmlit.py
@@ -11,5 +11,5 @@ my_cur = my_cnx.cursor()
 # Run query to test
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
